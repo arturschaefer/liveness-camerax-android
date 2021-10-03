5966f59b8165e61ae4bcfc2f5b3d6cfd3676a949
DESKTOP-JFU8E24\Artur Schaefer <artur.schaefer2@gmail.com>
2021-10-03 21:40:44 +0200

    chore: create state and action implementations

---
+++ livenesscamerax/src/main/java/com/schaefer/livenesscamerax/presentation/viewmodel/LivenessAction.kt
@@ -0,0 +1,9 @@
+package com.schaefer.livenesscamerax.presentation.viewmodel
+
+import com.schaefer.livenesscamerax.core.viewmodel.UIAction
+
+internal sealed class LivenessAction: UIAction {
+    object LivenessCompleted : LivenessAction()
+    object LivenessCanceled : LivenessAction()
+    data class EnableCameraButton (val enableButton: Boolean): LivenessAction()
+}
