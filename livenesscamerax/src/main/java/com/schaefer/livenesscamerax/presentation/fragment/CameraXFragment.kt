6359fbe058bd9ef77ecb6da114336d43c5528607
DESKTOP-JFU8E24\Artur Schaefer <artur.schaefer2@gmail.com>
2021-10-05 08:29:42 +0200

    refactor: remove unused code inside fragment

--- livenesscamerax/src/main/java/com/schaefer/livenesscamerax/presentation/fragment/CameraXFragment.kt
+++ livenesscamerax/src/main/java/com/schaefer/livenesscamerax/presentation/fragment/CameraXFragment.kt
@@ -1,39 +1,67 @@
 package com.schaefer.livenesscamerax.presentation.fragment
 
 import android.Manifest
-import android.content.pm.PackageManager
-import android.net.Uri
 import android.os.Bundle
-import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.Toast
-import androidx.camera.core.*
-import androidx.camera.lifecycle.ProcessCameraProvider
-import androidx.core.app.ActivityCompat
-import androidx.core.content.ContextCompat
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.core.view.isVisible
 import androidx.fragment.app.Fragment
+import androidx.fragment.app.viewModels
 import com.schaefer.livenesscamerax.R
-import com.schaefer.livenesscamerax.core.LuminosityAnalyzer
+import com.schaefer.livenesscamerax.camera.CameraX
+import com.schaefer.livenesscamerax.camera.CameraXCallback
+import com.schaefer.livenesscamerax.camera.CameraXCallbackImpl
+import com.schaefer.livenesscamerax.camera.CameraXImpl
+import com.schaefer.livenesscamerax.core.extensions.shouldShowRequest
+import com.schaefer.livenesscamerax.core.extensions.snack
 import com.schaefer.livenesscamerax.databinding.FragmentCameraxBinding
+import com.schaefer.livenesscamerax.domain.model.AnalyzeType
+import com.schaefer.livenesscamerax.domain.model.CameraSettings
+import com.schaefer.livenesscamerax.presentation.viewmodel.LivenessViewModel
+import kotlinx.coroutines.ExperimentalCoroutinesApi
+import kotlinx.coroutines.FlowPreview
+import kotlinx.coroutines.InternalCoroutinesApi
+import timber.log.Timber
 import java.io.File
-import java.text.SimpleDateFormat
-import java.util.*
-import java.util.concurrent.ExecutorService
-import java.util.concurrent.Executors
 
-class CameraXFragment : Fragment(R.layout.fragment_camerax) {
-
-    private var imageCapture: ImageCapture? = null
-    private lateinit var outputDirectory: File
-    private lateinit var cameraExecutor: ExecutorService
-
+@ExperimentalCoroutinesApi
+@FlowPreview
+@InternalCoroutinesApi
+internal class CameraXFragment : Fragment(R.layout.fragment_camerax) {
     private var _binding: FragmentCameraxBinding? = null
-
-    // This property is only valid between onCreateView and
-// onDestroyView.
     private val binding get() = _binding!!
+
+    private val livenessViewModel: LivenessViewModel by viewModels()
+
+    private val cameraXCallback: CameraXCallback by lazy {
+        CameraXCallbackImpl(::handlePictureSuccess, ::handlePictureError, requireContext())
+    }
+
+    private val cameraX: CameraX by lazy {
+        CameraXImpl(
+            settings = CameraSettings(),
+            cameraXCallback = cameraXCallback,
+            analyzeType = AnalyzeType.FACE_PROCESSOR,
+            lifecycleOwner = this,
+            context = requireContext()
+        )
+    }
+
+    private val cameraManifest = Manifest.permission.CAMERA
+    private val cameraPermission =
+        registerForActivityResult(ActivityResultContracts.RequestPermission()) { granted ->
+            with(binding.clRoot) {
+                when {
+                    granted -> permissionIsGranted()
+                    requireActivity().shouldShowRequest(cameraManifest) -> {
+                        snack(context.getString(R.string.message_permission_denied))
+                    }
+                    else -> snack(context.getString(R.string.message_permission_unknown))
+                }
+            }
+        }
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -41,148 +69,46 @@ class CameraXFragment : Fragment(R.layout.fragment_camerax) {
         savedInstanceState: Bundle?
     ): View? {
         _binding = FragmentCameraxBinding.inflate(inflater, container, false)
-        val view = binding.root
-        return view
+        return binding.root
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
-        if (allPermissionsGranted()) {
-            startCamera()
-        } else {
-            ActivityCompat.requestPermissions(
-                requireActivity(), REQUIRED_PERMISSIONS, REQUEST_CODE_PERMISSIONS
-            )
-        }
-
-        // Set up the listener for take photo button
-        binding.cameraCaptureButton.setOnClickListener { takePhoto() }
-
-        outputDirectory = getOutputDirectory()
-
-        cameraExecutor = Executors.newSingleThreadExecutor()
+        cameraPermission.launch(cameraManifest)
     }
 
-    private fun takePhoto() {
-        // Get a stable reference of the modifiable image capture use case
-        val imageCapture = imageCapture ?: return
-
-        // Create time-stamped output file to hold the image
-        val photoFile = File(
-            outputDirectory,
-            SimpleDateFormat(
-                FILENAME_FORMAT, Locale.US
-            ).format(System.currentTimeMillis()) + ".jpg"
-        )
-
-        // Create output options object which contains file + metadata
-        val outputOptions = ImageCapture.OutputFileOptions.Builder(photoFile).build()
-
-        // Set up image capture listener, which is triggered after photo has
-        // been taken
-        imageCapture.takePicture(
-            outputOptions,
-            ContextCompat.getMainExecutor(requireContext()),
-            object : ImageCapture.OnImageSavedCallback {
-                override fun onError(exc: ImageCaptureException) {
-                    Log.e(TAG, "Photo capture failed: ${exc.message}", exc)
-                }
-
-                override fun onImageSaved(output: ImageCapture.OutputFileResults) {
-                    val savedUri = Uri.fromFile(photoFile)
-                    val msg = "Photo capture succeeded: $savedUri"
-                    Toast.makeText(requireContext(), msg, Toast.LENGTH_SHORT).show()
-                    Log.d(TAG, msg)
-                }
-            })
+    private fun permissionIsGranted() {
+        startCamera()
     }
 
     private fun startCamera() {
-        val cameraProviderFuture = ProcessCameraProvider.getInstance(requireContext())
-
-        cameraProviderFuture.addListener(Runnable {
-            // Used to bind the lifecycle of cameras to the lifecycle owner
-            val cameraProvider: ProcessCameraProvider = cameraProviderFuture.get()
-
-            // Preview
-            val preview = Preview.Builder()
-                .build()
-                .also {
-                    it.setSurfaceProvider(binding.viewFinder.surfaceProvider)
-                }
-
-            imageCapture = ImageCapture.Builder()
-                .build()
-
-            val imageAnalyzer = ImageAnalysis.Builder()
-                .build()
-                .also {
-                    it.setAnalyzer(cameraExecutor, LuminosityAnalyzer { luma ->
-                        Log.d(TAG, "Average luminosity: $luma")
-                    })
-                }
-
-
-            // Select back camera as a default
-            val cameraSelector = CameraSelector.DEFAULT_BACK_CAMERA
-
-            try {
-                // Unbind use cases before rebinding
-                cameraProvider.unbindAll()
-
-                // Bind use cases to camera
-                cameraProvider.bindToLifecycle(
-                    this, cameraSelector, preview, imageCapture, imageAnalyzer)
-            } catch (exc: Exception) {
-                Log.e(TAG, "Use case binding failed", exc)
-            }
-
-        }, ContextCompat.getMainExecutor(requireContext()))
-    }
-
-    private fun allPermissionsGranted() = REQUIRED_PERMISSIONS.all {
-        ContextCompat.checkSelfPermission(
-            requireContext(), it
-        ) == PackageManager.PERMISSION_GRANTED
-    }
-
-    private fun getOutputDirectory(): File {
-        val mediaDir = activity?.externalMediaDirs?.firstOrNull()?.let {
-            File(it, resources.getString(R.string.app_name)).apply { mkdirs() }
+        binding.overlayView.apply {
+            init()
+            invalidate()
+            isVisible = true
         }
-        return if (mediaDir != null && mediaDir.exists())
-            mediaDir else activity?.filesDir!!
-    }
 
-    override fun onRequestPermissionsResult(
-        requestCode: Int, permissions: Array<String>, grantResults:
-        IntArray
-    ) {
-        if (requestCode == REQUEST_CODE_PERMISSIONS) {
-            if (allPermissionsGranted()) {
-                startCamera()
-            } else {
-                Toast.makeText(
-                    requireContext(),
-                    "Permissions not granted by the user.",
-                    Toast.LENGTH_SHORT
-                ).show()
-                activity?.finish()
-            }
+        cameraX.startCamera(binding.viewFinder)
+
+        livenessViewModel.let {
+            it.observeFacesDetection(cameraX.getFacesFlowable())
+            it.observeLuminosity(cameraX.getLuminosity())
+        }
+
+        lifecycle.addObserver(cameraX.getLifecycleObserver())
+
+        binding.cameraCaptureButton.setOnClickListener {
+            cameraX.takePicture()
         }
     }
 
-    override fun onDestroy() {
-        super.onDestroy()
-        _binding = null
-        cameraExecutor.shutdown()
+
+    private fun handlePictureError(throwable: Throwable) {
+        Timber.e(throwable)
     }
 
-    companion object {
-        private const val TAG = "CameraXBasic"
-        private const val FILENAME_FORMAT = "yyyy-MM-dd-HH-mm-ss-SSS"
-        private const val REQUEST_CODE_PERMISSIONS = 10
-        private val REQUIRED_PERMISSIONS = arrayOf(Manifest.permission.CAMERA)
+    private fun handlePictureSuccess(file: File) {
+        Timber.d(file.path)
     }
 }
