e4c206e9defc9784e3ff066d32714418a57ce223
DESKTOP-JFU8E24\Artur Schaefer <artur.schaefer2@gmail.com>
2021-10-19 20:32:28 +0200

    LIVE-7: rotate image and compress file

---
+++ livenesscamerax/src/main/java/com/schaefer/livenesscamerax/camera/provider/RotateImageProvider.kt
@@ -0,0 +1,8 @@
+package com.schaefer.livenesscamerax.camera.provider
+
+import java.io.File
+
+internal interface RotateImageProvider {
+
+    fun editPhotoFile(photoFile: File)
+}
---
+++ livenesscamerax/src/main/java/com/schaefer/livenesscamerax/camera/provider/RotateImageProviderImpl.kt
@@ -0,0 +1,159 @@
+package com.schaefer.livenesscamerax.camera.provider
+
+import android.content.Context
+import android.graphics.Bitmap
+import android.graphics.BitmapFactory
+import android.graphics.Matrix
+import android.net.Uri
+import android.os.Build
+import androidx.core.net.toUri
+import androidx.exifinterface.media.ExifInterface
+import java.io.ByteArrayOutputStream
+import java.io.File
+import java.io.IOException
+import java.io.InputStream
+import kotlin.math.roundToInt
+
+// Reference https://stackoverflow.com/questions/14066038/why-does-an-image-captured-using-camera-intent-gets-rotated-on-some-devices-on-a
+
+private const val MAX_HEIGHT = 1024
+private const val MAX_WIDTH = 1024
+private const val ORIENTATION_90 = 90F
+private const val ORIENTATION_180 = 180F
+private const val ORIENTATION_270 = 270F
+private const val MARSHMALLOW = 23
+private const val IMAGE_COMPRESS_QUALITY = 80
+
+internal class RotateImageProviderImpl(private val context: Context) : RotateImageProvider {
+
+    override fun editPhotoFile(photoFile: File) {
+        runCatching {
+            val bitmap = handleSamplingAndRotationBitmap(photoFile.toUri())
+            val bos = ByteArrayOutputStream().also {
+                bitmap?.compress(Bitmap.CompressFormat.JPEG, IMAGE_COMPRESS_QUALITY, it)
+            }
+            photoFile.writeBytes(bos.toByteArray())
+        }
+    }
+
+    /**
+     * This method is responsible for solving the rotation issue if exist. Also scale the images to
+     * 1024x1024 resolution
+     *
+     * @param context       The current context
+     * @param selectedImage The Image URI
+     * @return Bitmap image results
+     * @throws IOException
+     */
+    @Throws(IOException::class)
+    fun handleSamplingAndRotationBitmap(selectedImage: Uri): Bitmap? {
+
+        // First decode with inJustDecodeBounds=true to check dimensions
+        val options = BitmapFactory.Options()
+        options.inJustDecodeBounds = true
+        var imageStream: InputStream? = context.contentResolver.openInputStream(selectedImage)
+        BitmapFactory.decodeStream(imageStream, null, options)
+        imageStream?.close()
+
+        // Calculate inSampleSize
+        options.inSampleSize = calculateInSampleSize(options, MAX_WIDTH, MAX_HEIGHT)
+
+        // Decode bitmap with inSampleSize set
+        options.inJustDecodeBounds = false
+        imageStream = context.contentResolver.openInputStream(selectedImage)
+        var img = BitmapFactory.decodeStream(imageStream, null, options)
+        img = img?.let { rotateImageIfRequired(it, selectedImage) }
+
+        return img
+    }
+
+    /**
+     * Calculate an inSampleSize for use in a [BitmapFactory.Options] object when decoding
+     * bitmaps using the decode* methods from [BitmapFactory]. This implementation calculates
+     * the closest inSampleSize that will result in the final decoded bitmap having a width and
+     * height equal to or larger than the requested width and height. This implementation does not
+     * ensure a power of 2 is returned for inSampleSize which can be faster when decoding but
+     * results in a larger bitmap which isn't as useful for caching purposes.
+     *
+     * @param options   An options object with out* params already populated (run through a decode*
+     * method with inJustDecodeBounds==true
+     * @param reqWidth  The requested width of the resulting bitmap
+     * @param reqHeight The requested height of the resulting bitmap
+     * @return The value to be used for inSampleSize
+     */
+    private fun calculateInSampleSize(
+        options: BitmapFactory.Options,
+        reqWidth: Int,
+        reqHeight: Int
+    ): Int {
+        // Raw height and width of image
+        val height = options.outHeight
+        val width = options.outWidth
+        var inSampleSize = 1
+        if (height > reqHeight || width > reqWidth) {
+
+            // Calculate ratios of height and width to requested height and width
+            val heightRatio = (height.toFloat() / reqHeight.toFloat()).roundToInt()
+            val widthRatio = (width.toFloat() / reqWidth.toFloat()).roundToInt()
+
+            // Choose the smallest ratio as inSampleSize value, this will guarantee a final image
+            // with both dimensions larger than or equal to the requested height and width.
+            inSampleSize = if (heightRatio < widthRatio) heightRatio else widthRatio
+
+            // This offers some additional logic in case the image has a strange
+            // aspect ratio. For example, a panorama may have a much larger
+            // width than height. In these cases the total pixels might still
+            // end up being too large to fit comfortably in memory, so we should
+            // be more aggressive with sample down the image (=larger inSampleSize).
+            val totalPixels = (width * height).toFloat()
+
+            // Anything more than 2x the requested pixels we'll sample down further
+            val totalReqPixelsCap = (reqWidth * reqHeight * 2).toFloat()
+            while (totalPixels / (inSampleSize * inSampleSize) > totalReqPixelsCap) {
+                inSampleSize++
+            }
+        }
+        return inSampleSize
+    }
+
+    /**
+     * Rotate an image if required.
+     *
+     * @param img           The image bitmap
+     * @param selectedImage Image URI
+     * @return The resulted Bitmap after manipulation
+     */
+    @Throws(IOException::class)
+    fun rotateImageIfRequired(img: Bitmap, selectedImage: Uri): Bitmap? {
+        val input = context.contentResolver.openInputStream(selectedImage)
+        val exifInterface = getExifInterface(input, selectedImage)
+
+        return exifInterface?.let {
+            when (
+                it.getAttributeInt(ExifInterface.TAG_ORIENTATION, ExifInterface.ORIENTATION_NORMAL)
+            ) {
+                ExifInterface.ORIENTATION_ROTATE_90 -> rotateImage(img, ORIENTATION_90)
+                ExifInterface.ORIENTATION_ROTATE_180 -> rotateImage(img, ORIENTATION_180)
+                ExifInterface.ORIENTATION_ROTATE_270 -> rotateImage(img, ORIENTATION_270)
+                else -> img
+            }
+        }
+    }
+
+    private fun getExifInterface(
+        input: InputStream?,
+        selectedImage: Uri
+    ) = input.takeIf { it != null && Build.VERSION.SDK_INT > MARSHMALLOW }?.let { inputStream ->
+        ExifInterface(inputStream)
+    } ?: selectedImage.path?.let { path ->
+        ExifInterface(path)
+    }
+
+    private fun rotateImage(img: Bitmap, degree: Float): Bitmap? {
+        val matrix = Matrix()
+        matrix.postRotate(degree)
+        val rotatedImg = Bitmap.createBitmap(img, 0, 0, img.width, img.height, matrix, true)
+        img.recycle()
+        return rotatedImg
+    }
+}
