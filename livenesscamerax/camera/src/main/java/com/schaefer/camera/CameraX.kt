be2135995f1783d0f26508c778bfeb4af110ca41
DESKTOP-JFU8E24\Artur Schaefer <artur.schaefer2@gmail.com>
2021-11-15 17:55:25 +0100

    LIVE-29: moved CameraX.kt

--- livenesscamerax/camera/src/main/java/com/schaefer/camera/CameraX.kt
+++ livenesscamerax/camera/src/main/java/com/schaefer/camera/CameraX.kt
@@ -1,11 +1,11 @@
-package com.schaefer.livenesscamerax.camera
+package com.schaefer.camera
 
 import androidx.camera.view.PreviewView
 import androidx.lifecycle.LifecycleObserver
-import com.schaefer.livenesscamerax.domain.model.FaceResult
+import com.schaefer.camera.model.FaceResult
 import kotlinx.coroutines.flow.Flow
 
-internal interface CameraX {
+interface CameraX {
     fun startCamera(cameraPreviewView: PreviewView)
     fun enableFlash(enabled: Boolean)
     fun takePicture(takenByUser: Boolean)
