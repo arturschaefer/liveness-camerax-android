878dcda5332c8aa1fa9186b6c45e729c0e9f9f12
DESKTOP-JFU8E24\Artur Schaefer <artur.schaefer2@gmail.com>
2021-11-19 08:15:52 +0100

    LIVE-31: adjust DI modules

--- livenesscamerax/src/main/java/com/schaefer/livenesscamerax/di/LivenessCameraXContainer.kt
+++ livenesscamerax/src/main/java/com/schaefer/livenesscamerax/di/LivenessCameraXContainer.kt
@@ -2,26 +2,15 @@ package com.schaefer.livenesscamerax.di
 
 import android.app.Application
 import android.content.Context
-import androidx.lifecycle.LifecycleOwner
-import com.schaefer.camera.CameraX
-import com.schaefer.camera.core.callback.CameraXCallback
 import com.schaefer.camera.domain.model.FaceResult
 import com.schaefer.camera.domain.repository.checkliveness.CheckLivenessRepositoryFactory
-import com.schaefer.camera.domain.repository.file.FileRepositoryFactory
 import com.schaefer.camera.domain.repository.resultliveness.ResultLivenessRepositoryFactory
-import com.schaefer.camera.domain.usecase.EditPhotoUseCaseFactory
 import com.schaefer.core.resourceprovider.ResourcesProvider
 import com.schaefer.core.resourceprovider.ResourcesProviderFactory
-import com.schaefer.domain.EditPhotoUseCase
 import com.schaefer.domain.model.PhotoResultDomain
 import com.schaefer.domain.repository.CheckLivenessRepository
-import com.schaefer.domain.repository.FileRepository
 import com.schaefer.domain.repository.ResultLivenessRepository
-import com.schaefer.livenesscamerax.camera.CameraXImpl
-import com.schaefer.livenesscamerax.domain.model.StorageType
-import com.schaefer.livenesscamerax.domain.model.toDomain
-import com.schaefer.livenesscamerax.presentation.model.CameraSettings
-import com.schaefer.livenesscamerax.presentation.navigation.LivenessEntryPoint
+import com.schaefer.livenesscamerax.navigation.LivenessEntryPoint
 
 internal class LivenessCameraXContainer(private val application: Application) {
     private val provideLivenessEntryPoint = LivenessEntryPoint
@@ -34,10 +23,6 @@ internal class LivenessCameraXContainer(private val application: Application) {
         return ResourcesProviderFactory(provideContext()).create()
     }
 
-    fun provideEditPhotoUseCase(): EditPhotoUseCase {
-        return EditPhotoUseCaseFactory.create()
-    }
-
     fun provideResultLivenessRepository(): ResultLivenessRepository<PhotoResultDomain> {
         return ResultLivenessRepositoryFactory.apply {
             resultCallback = provideLivenessEntryPoint::postResultCallback
@@ -46,22 +31,5 @@ internal class LivenessCameraXContainer(private val application: Application) {
 
     fun provideCheckLivenessRepository(): CheckLivenessRepository<FaceResult> {
         return CheckLivenessRepositoryFactory.create()
-    }
-
-    private fun provideFileRepository(storageType: StorageType): FileRepository {
-        return FileRepositoryFactory.apply { this.storageType = storageType.toDomain() }.create()
-    }
-
-    fun provideCameraX(
-        cameraSettings: CameraSettings,
-        cameraXCallback: CameraXCallback,
-        lifecycleOwner: LifecycleOwner
-    ): CameraX {
-        return CameraXImpl(
-            settings = cameraSettings,
-            cameraXCallback = cameraXCallback,
-            lifecycleOwner = lifecycleOwner,
-            fileRepository = provideFileRepository(cameraSettings.storageType)
-        )
     }
 }
