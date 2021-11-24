5805a816a45e78ac76a274f08013cf77c5266f9f
DESKTOP-JFU8E24\Artur Schaefer <artur.schaefer2@gmail.com>
2021-11-24 22:41:29 +0100

    LIVE-33: change CoroutineScope injection

--- livenesscamerax/camera/src/main/java/com/schaefer/camera/core/processor/face/FaceFrameProcessorFactory.kt
+++ livenesscamerax/camera/src/main/java/com/schaefer/camera/core/processor/face/FaceFrameProcessorFactory.kt
@@ -1,18 +1,14 @@
 package com.schaefer.camera.core.processor.face
 
-import androidx.lifecycle.lifecycleScope
 import com.schaefer.camera.core.detector.VisionFaceDetector
-import com.schaefer.camera.di.CameraModule
 import com.schaefer.camera.di.CameraModule.container
 import com.schaefer.core.factory.Factory
-import kotlinx.coroutines.CoroutineScope
 
 internal object FaceFrameProcessorFactory : Factory<FaceFrameProcessor> {
 
-    private val coroutineScope: CoroutineScope = CameraModule.lifecycleOwner.lifecycleScope
     private val detector: VisionFaceDetector = container.provideVisionFaceDetector()
 
     override fun create(): FaceFrameProcessor {
-        return FaceFrameProcessorImpl(coroutineScope, detector)
+        return FaceFrameProcessorImpl(container.provideCoroutineScope(), detector)
     }
 }
